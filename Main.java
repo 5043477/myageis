5120b21bc999d751997d60457bb696bd9147c01d
bwhitmore7 <bryson.whitmore@patelhs.org>
2020-09-22 16:40:10 +0000

    everything

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
-class Main {
+public class Main {
   public static void main(String[] args) {
     int myage = 14;
+    int mybirthday = 03/17/2006;
+    int todaysdate = 9/22/2020;
 
-
-    System.out.println("i am 14" + myage + "years old")
+      System.out.println("i am " + 14 + " years old.");
   }
 }
